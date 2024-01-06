42e02bc2786ede31e2841687d4b86d18f8a2970a
conrado noe <conradonoee@gmail.com>
2024-01-06 11:26:42 -0300

    re-entrega final javascript terminado

--- javascript/formulario.js
+++ javascript/formulario.js
@@ -1,54 +1,142 @@
- // entrega 3
-//formulario con guardado en local storage
-// const iniciarForm = document.getElementById("iniciar-form")
-// const inputNameForm = document.getElementById("nameForm")
-// const inputUserForm = document.getElementById("userForm")
-// const inputEmailForm = document.getElementById("emailForm")
-// const inputPasswordForm = document.getElementById("passwordForm")
-// const inputButtonForm = document.getElementById("sendData")
-// const DatosTerminados = document.getElementById("datosGuardadosBien")
-// // nuevos botones
-// const nuevobotton = document.getElementById("boton-nuevo")
-// const nuevoBoton2 = document.getElementById("boton-nuevo2")
+
+// formulario con guardado en local storage
+const iniciarForm = document.getElementById("iniciar-form")
+const inputNameForm = document.getElementById("nameForm")
+const inputUserForm = document.getElementById("userForm")
+const inputEmailForm = document.getElementById("emailForm")
+const inputPasswordForm = document.getElementById("passwordForm")
+const inputButtonForm = document.getElementById("sendData")
+const DatosTerminados = document.getElementById("datosGuardadosBien")
+// nuevos botones
+const nuevobotton = document.getElementById("boton-nuevo")
+const nuevoBoton2 = document.getElementById("boton-nuevo2")
 
 // recupero los datos al cargar la página
-// window.onload = function() {
-//     const datosGuardados = localStorage.getItem("DatosDelForm");
-//     if (datosGuardados) {
-//         const datos = JSON.parse(datosGuardados);
-//         DatosTerminados.innerHTML = "Bienvenido " + datos.nombre + ". Tus datos han sido guardados correctamente.";
-//     }
-// }
+window.onload = function() {
+    const datosGuardados = localStorage.getItem("DatosDelForm");
+    if (datosGuardados) {
+        const datos = JSON.parse(datosGuardados);
+        DatosTerminados.innerHTML = "Bienvenido " + datos.nombre + ". Tus datos han sido guardados correctamente.";
+    }
+}
 
-// inputButtonForm.addEventListener("click", function(event){
-//     event.preventDefault();
+inputButtonForm.addEventListener("click", function(event){
+    event.preventDefault();
     
-//     let DatosDelForm = {
-//         nombre: inputNameForm.value,
-//         usuario: inputUserForm.value,
-//         email: inputEmailForm.value,
-//         contrasenia: inputPasswordForm.value,
-//     }
+    let DatosDelForm = {
+        nombre: inputNameForm.value,
+        usuario: inputUserForm.value,
+        email: inputEmailForm.value,
+        contrasenia: inputPasswordForm.value,
+    }
 
-//     if (Object.values(DatosDelForm).some(value => !value.trim())) {
-//         Swal.fire({
-//             icon: "error",
-//             title: "lo siento",
-//             text: "completa tus datos correctamente",
-//             footer: '<a href="#">Why do I have this issue?</a>'
-//             });;
-//         return;
-//     } else {
-//         Swal.fire({
-//             position: "center",
-//             icon: "success",
-//             title: "tus datos han sido guardados correctamente",
-//             showConfirmButton: false,
-//             timer: 1500
-//             });;
-//     }
+    if (Object.values(DatosDelForm).some(value => !value.trim())) {
+        Swal.fire({
+            icon: "error",
+            title: "lo siento",
+            text: "completa tus datos correctamente",
+            footer: '<a href="#">Why do I have this issue?</a>'
+            });;
+        return;
+    } else {
+        Swal.fire({
+            position: "center",
+            icon: "success",
+            title: "tus datos han sido guardados correctamente",
+            showConfirmButton: false,
+            timer: 1500
+            });;
+    }
 
-//     DatosTerminados.innerHTML = "Bienvenido " + DatosDelForm.nombre + ". Tus datos han sido guardados correctamente";
-//     let resultado = JSON.stringify(DatosDelForm);
-//     localStorage.setItem("DatosDelForm", resultado);
-// });
+    DatosTerminados.innerHTML = "Bienvenido " + DatosDelForm.nombre + ". Tus datos han sido guardados correctamente";
+    let resultado = JSON.stringify(DatosDelForm);
+    localStorage.setItem("DatosDelForm", resultado);
+});
+
+
+
+// clima api
+
+const result = document.querySelector('.result');
+const formApi = document.querySelector('.get-weather');
+const nameCity = document.querySelector('#city');
+const nameCountry = document.querySelector('#country');
+
+formApi.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (nameCity.value === '' || nameCountry.value === '') {
+        showError('Ambos campos son obligatorios...');
+        return;
+    }
+
+    callAPI(nameCity.value, nameCountry.value);
+    
+})
+
+function callAPI(city, country){
+    const apiId = 'd6558a0b932da6e03c088f4b10c7173a';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiId}`
+
+    fetch(url)
+        .then(data => {
+            return data.json();
+        })
+        .then(dataJSON => {
+            if (dataJSON.cod === '404') {
+                showError('Ciudad no encontrada...');
+            } else {
+                clearHTML();
+                showWeather(dataJSON);
+            }
+            
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+
+function showWeather(data){
+    const {name, main:{temp, temp_min, temp_max}, weather:[arr]} = data;
+
+    const degrees = kelvinToCentigrade(temp);
+    const min = kelvinToCentigrade(temp_min);
+    const max = kelvinToCentigrade(temp_max);
+
+    const content = document.createElement('div');
+    content.innerHTML = `
+        <h5>Clima en ${name}</h5>
+        <img src="https://openweathermap.org/img/wn/${arr.icon}@2x.png" alt="icon">
+        <h2>${degrees}°C</h2>
+        <p>Max: ${max}°C</p>
+        <p>Min: ${min}°C</p>
+    `;
+
+    result.appendChild(content);
+
+    console.log(name);
+    console.log(temp);
+    console.log(temp_max);
+    console.log(temp_min);
+    console.log(arr.icon); 
+}
+
+function showError(message){
+    //console.log(message);
+    const alert = document.createElement('p');
+    alert.classList.add('alert-message');
+    alert.innerHTML = message;
+
+    formApi.appendChild(alert);
+    setTimeout(() => {
+        alert.remove();
+    }, 3000);
+}
+
+function kelvinToCentigrade(temp){
+    return parseInt(temp - 273.15);
+}
+
+function clearHTML(){
+    result.innerHTML = '';
+}
--- javascript/main.js
+++ javascript/main.js
@@ -11,17 +11,17 @@ const Producto = function(id,tipoDeProducto, marca, tamanio, precio,img,stock) {
 }
 //creo productos
 let producto1 = new Producto(1,"Tabla", "pyzel", "6.0 fts", 700, `../assets/tabla-pyzel-1.jpeg`,1 );
-let producto2 = new Producto(2,"Tabla", "pyzel", "6.0 fts", 900, `../assets/tabla-pyzel-2.webp`,1);
-let producto3 = new Producto(3,"Tabla", "lost", "6.0 fts", 1000,`../assets/tabla-lost-1.jpeg`,1);
-let producto4 = new Producto(4,"Tabla", "lost", "6.0 fts", 1200,`../assets/tabla-lost-2.webp`,1);
-let producto5 = new Producto(5,"Traje de neoprene", "oneil", "6.0 fts", 400,`../assets/trajeDeNeoprene-oneill-1.webp`,1);
-let producto6 = new Producto(6,"Traje de neoprene", "oneil", "6.0 fts", 250,`../assets/trajeDeNeoprene-oneill-2.webp`,1);
-let producto7 = new Producto(7,"Traje de neoprene", "oneil", "6.0 fts", 380,"../assets/trajeDeNeoprene-billabong-1.jpeg",1);
-let producto8 = new Producto(8,"Traje de neoprene", "oneil", "6.0 fts", 300,"../assets/trajeDeNeoprene-billabong-2.webp",1);
-let producto9 = new Producto(9,"Quillas", "oneil", "6.0 fts", 100 ,"../assets/quillas-fcs-1.webp",1);
-let producto10 = new Producto(10,"Quillas", "oneil", "6.0 fts", 120 ,"../assets/quillas-fcs-2.webp",1);
-let producto11 = new Producto(11,"Quillas", "oneil", "6.0 fts", 110 ,"../assets/quillas-fcs-3.jpeg",1);
-let producto12 = new Producto(12,"Quillas", "oneil", "6.0 fts", 90 ,"../assets/quillas-fcs-4.jpeg",1)
+let producto2 = new Producto(2,"Tabla", "pyzel", "5.9 fts", 900, `../assets/tabla-pyzel-2.webp`,1);
+let producto3 = new Producto(3,"Tabla", "lost", "6.4 fts", 1000,`../assets/tabla-lost-1.jpeg`,1);
+let producto4 = new Producto(4,"Tabla", "lost", "5.8 fts", 1200,`../assets/tabla-lost-2.webp`,1);
+let producto5 = new Producto(5,"Traje de neoprene", "oneil", "  XL ", 400,`../assets/trajeDeNeoprene-oneill-1.webp`,1);
+let producto6 = new Producto(6,"Traje de neoprene", "oneil", " XT ", 250,`../assets/trajeDeNeoprene-oneill-2.webp`,1);
+let producto7 = new Producto(7,"Traje de neoprene", "billabonf", " MT ", 380,"../assets/trajeDeNeoprene-billabong-1.jpeg",1);
+let producto8 = new Producto(8,"Traje de neoprene", "billabong", " XS ", 300,"../assets/trajeDeNeoprene-billabong-2.webp",1);
+let producto9 = new Producto(9,"Quillas", "fcs", " L", 100 ,"../assets/quillas-fcs-1.webp",1);
+let producto10 = new Producto(10,"Quillas", "fcs", "M", 120 ,"../assets/quillas-fcs-2.webp",1);
+let producto11 = new Producto(11,"Quillas", "fcs", "M", 110 ,"../assets/quillas-fcs-3.jpeg",1);
+let producto12 = new Producto(12,"Quillas", "fcs", "S", 90 ,"../assets/quillas-fcs-4.jpeg",1)
 
 let lista = [producto1, producto2, producto3, producto4, producto5, producto6, producto7,producto8,producto9,producto10,producto11,producto12];
 
